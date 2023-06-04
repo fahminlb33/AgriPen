57387f6fafc2291aa9e01b3deeae6b691f1056fe
fahminlb33 <fahminlb33@gmail.com>
2023-06-04 16:06:44 +0700

    chore: reorganize classes

--- agripen-backend/AgriPen.Domain/Entities/Disease.cs
+++ agripen-backend/AgriPen.Domain/Entities/Disease.cs
@@ -27,3 +27,21 @@ public class DiseasePrediction : IEntityModel
     public GpsAddress GpsAddress { get; set; }
     public LocalAddress LocalAddress { get; set; }
 }
+
+public class DiseaseProbability
+{
+    [Key, StringLength(26)]
+    public Ulid Id { get; set; }
+
+    public double BacterialBlight { get; set; }
+    public double Blast { get; set; }
+    public double BrownSpot { get; set; }
+    public double Tungro { get; set; }
+    public double Healthy { get; set; }
+
+    [StringLength(26)]
+    public Ulid PredictionId { get; set; }
+
+    // navigation
+    public DiseasePrediction Prediction { get; set; }
+}
--- agripen-backend/AgriPen.Domain/Entities/LandObservation.cs
+++ agripen-backend/AgriPen.Domain/Entities/LandObservation.cs
@@ -20,3 +20,21 @@ public class LandObservation : IEntityModel
     public LocalAddress LocalAddress { get; set; }
     public List<Telemetry> Telemetries { get; set; }
 }
+
+public class Telemetry
+{
+    [Key, StringLength(26)]
+    public Ulid Id { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+    public double AirTemperature { get; set; }
+    public double AirHumidity { get; set; }
+    public double AirHeatIndex { get; set; }
+    public double SoilMoisture { get; set; }
+    public double SunIllumination { get; set; }
+
+    [StringLength(26)]
+    public Ulid ObservationId { get; set; }
+
+    // navigation
+    public LandObservation Observation { get; set; }
+}
